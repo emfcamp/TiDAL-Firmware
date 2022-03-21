6f56ec4734a44cddde9e1c8118e14cc2909d7d50
Matthew Wilkes <git@matthewwilkes.name>
2022-03-21 22:17:02 +0000

    Second build attempt
    Reduce the artifacts to just the core three binaries and disable SPIRAM
    temporarily.

--- YKW/mpconfigboard.cmake
+++ YKW/mpconfigboard.cmake
@@ -3,7 +3,7 @@ set(IDF_TARGET esp32s3)
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.usb
-    boards/sdkconfig.spiram_sx
+    boards/sdkconfig.ble
     boards/YKW/sdkconfig.board
 )
 
