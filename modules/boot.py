a7bea7ebdd51c6f382006ce397d9b232a8c7319d
Matthew Wilkes <git@matthewwilkes.name>
2022-04-08 23:10:48 +0100

    Configure the badge to enable webrepl and an access point by default, to aid testing while we get ACM console working.

--- modules/boot.py
+++ modules/boot.py
@@ -2,3 +2,35 @@ import _thread
 import joystick
 
 _thread.start_new_thread(joystick.joystick_active, ())
+
+try:
+    open("webrepl_cfg.py")
+except OSError:
+    # No webrepl password is set, use aaaa
+    with open("webrepl_cfg.py", "wt", encoding="utf-8") as cfg:
+        cfg.write("PASS = 'tilda'\n")
+        
+
+try:
+    open("wifi_cfg.py")
+except OSError:
+    # No webrepl password is set, use aaaa
+    with open("wifi_cfg.py", "wt", encoding="utf-8") as cfg:
+        cfg.write("""
+import network
+
+ssid = "tilda"
+password = "tilda"
+
+ap = network.WLAN(network.AP_IF)
+ap.active(True)
+ap.config(essid=ssid, password=password)
+""")
+
+
+import wifi_cfg
+
+import esp
+esp.osdebug(None)
+import webrepl
+webrepl.start()
