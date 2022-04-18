282adf2c5aeb052b48f35872042838f600a54759
Matthew Wilkes <git@matthewwilkes.name>
2022-04-18 18:50:48 +0100

    Add lvgl upy bindings as a driver

--- drivers/micropython.cmake
+++ drivers/micropython.cmake
@@ -1,7 +1,5 @@
-
 # Add the ESP IOT add-on
 include(${CMAKE_CURRENT_LIST_DIR}/../esp-iot-solution/component.cmake)
-
 
 # Add the HID interface
 include(${CMAKE_CURRENT_LIST_DIR}/tidal_usb/micropython.cmake)
@@ -9,4 +7,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/tidal_usb/micropython.cmake)
 # Add the st7789 driver
 include(${CMAKE_CURRENT_LIST_DIR}/../st7789/st7789/micropython.cmake)
 
+# Add the graphics library
+set(ESP_PLATFORM false)
+include(${CMAKE_CURRENT_LIST_DIR}/lv_binding_micropython/bindings.cmake)
 
