3363c4958f8ebbb9c794bb69928a004d6266dae6
Tom Sutcliffe <tomsci@me.com>
2022-04-24 14:11:06 +0100

    Don't include "-dirty" in the firmware version
    As our build process always dirties submodules, so showing it doesn't
    help with anything.

--- tildamk6/mpconfigboard.cmake
+++ tildamk6/mpconfigboard.cmake
@@ -1,6 +1,13 @@
 set(IDF_TARGET esp32s3)
 
 git_describe("TIDAL_GIT_VERSION" "${MICROPY_PORT_DIR}/../../../")
+if(TIDAL_GIT_VERSION MATCHES "\\-dirty$")
+    # Our build process always dirties submodules, so it's not worth stating
+    # (And it's easier to strip here than fork git_describe())
+    string(LENGTH "${TIDAL_GIT_VERSION}" strlen)
+    math(EXPR strlen "${strlen} - 7")
+    string(SUBSTRING "${TIDAL_GIT_VERSION}" 0 ${strlen} TIDAL_GIT_VERSION)
+endif()
 message("TIDAL_GIT_VERSION=${TIDAL_GIT_VERSION}")
 configure_file("${MICROPY_PORT_DIR}/boards/tildamk6/sdkconfig.project_ver.in" "${MICROPY_PORT_DIR}/build-tildamk6/sdkconfig.project_ver")
 
