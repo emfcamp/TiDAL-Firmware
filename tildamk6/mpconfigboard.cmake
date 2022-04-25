69cd1166e38c56ef325efb85460e4feae75bdd56
Kliment <kliment@0xfb.com>
2022-04-25 19:46:38 +0200

    Add parametric SPIRAM init on devboard and production, but not proto

--- tildamk6/mpconfigboard.cmake
+++ tildamk6/mpconfigboard.cmake
@@ -10,16 +10,37 @@ if(TIDAL_GIT_VERSION MATCHES "\\-dirty$")
 endif()
 message("TIDAL_GIT_VERSION=${TIDAL_GIT_VERSION}")
 configure_file("${MICROPY_PORT_DIR}/boards/tildamk6/sdkconfig.project_ver.in" "${MICROPY_PORT_DIR}/build-tildamk6/sdkconfig.project_ver")
-
-set(SDKCONFIG_DEFAULTS
-    boards/sdkconfig.base
-    boards/sdkconfig.usb
-    boards/sdkconfig.240mhz
-    #boards/sdkconfig.spiram_sx
-    #boards/tildamk6/sdkconfig.spiram.board
-    boards/tildamk6/sdkconfig.board
-    build-tildamk6/sdkconfig.project_ver
-)
+if(DEFINED ENV{CONFIG_TIDAL_VARIANT_DEVBOARD})
+    set(SDKCONFIG_DEFAULTS
+        boards/sdkconfig.base
+        boards/sdkconfig.usb
+        boards/sdkconfig.240mhz
+        boards/sdkconfig.spiram_sx
+        boards/tildamk6/sdkconfig.spiram.board
+        boards/tildamk6/sdkconfig.board
+        build-tildamk6/sdkconfig.project_ver
+    )
+elseif (DEFINED ENV{CONFIG_TIDAL_VARIANT_PROTOTYPE})
+    set(SDKCONFIG_DEFAULTS
+        boards/sdkconfig.base
+        boards/sdkconfig.usb
+        boards/sdkconfig.240mhz
+        #boards/sdkconfig.spiram_sx
+        #boards/tildamk6/sdkconfig.spiram.board
+        boards/tildamk6/sdkconfig.board
+        build-tildamk6/sdkconfig.project_ver
+    )
+else()
+    set(SDKCONFIG_DEFAULTS
+        boards/sdkconfig.base
+        boards/sdkconfig.usb
+        boards/sdkconfig.240mhz
+        boards/sdkconfig.spiram_sx
+        boards/tildamk6/sdkconfig.spiram.board
+        boards/tildamk6/sdkconfig.board
+        build-tildamk6/sdkconfig.project_ver
+    )
+endif()
 
 if(NOT MICROPY_FROZEN_MANIFEST)
     set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/tildamk6/manifest.py)
