e310de7968a621c0316c1be068728bdb83ff2593
Kliment <kliment@0xfb.com>
2022-04-25 20:31:27 +0200

    Disable PSRAM in devboard for now

--- tildamk6/mpconfigboard.cmake
+++ tildamk6/mpconfigboard.cmake
@@ -15,8 +15,8 @@ if(DEFINED ENV{CONFIG_TIDAL_VARIANT_DEVBOARD})
         boards/sdkconfig.base
         boards/sdkconfig.usb
         boards/sdkconfig.240mhz
-        boards/sdkconfig.spiram_sx
-        boards/tildamk6/sdkconfig.spiram.board
+        #boards/sdkconfig.spiram_sx
+        #boards/tildamk6/sdkconfig.spiram.board
         boards/tildamk6/sdkconfig.board
         build-tildamk6/sdkconfig.project_ver
     )
