480bde14a2ff7caf42a26a85d05b95903157a3eb
Tom Sutcliffe <tomsci@me.com>
2022-04-27 11:12:38 +0100

    TEST: Just deepsleep for 10 seconds

--- modules/boot.py
+++ modules/boot.py
@@ -1,34 +1,16 @@
 import tidal
-from esp32 import Partition
+import ota
 
 # Initialize USB early on
 tidal.usb.initialize()
+ota.gpio_hold(14, False)
 tidal.init_lcd()
 
-if tidal.BUTTON_FRONT.value() == 0:
-    # Boot to the recovery menu
-    from bootmenu import BootMenu
-
-    menu = BootMenu()
-    Partition.mark_app_valid_cancel_rollback()
-    menu.run_sync()
-else:
-    import app
-    import time
-    import emf_png
-    from app_launcher import Launcher
-    import uasyncio
-
-    tidal.display.bitmap(emf_png, 0, 0)
-    time.sleep(0.5)
-
-    menu = Launcher()
-    
-    # If we've made it to here, any OTA update has _probably_ gone ok...
-    Partition.mark_app_valid_cancel_rollback()
-
-    async def main():
-        menu_task = uasyncio.create_task(menu.run())
-        app.task_coordinator.context_changed("menu")
-        await menu_task
-    uasyncio.run(main())
+from textwindow import TextWindow
+import machine
+win = TextWindow(tidal.WHITE, tidal.BLACK)
+tidal.display.fill(tidal.WHITE)
+win.println("Deepsleep test")
+win.println("10 seconds...")
+ota.gpio_hold(14, True)
+machine.deepsleep(10000)
