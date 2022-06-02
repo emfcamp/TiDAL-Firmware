f750eaf790233e8dfe6ca2f2458777bdbe18f766
Tom Sutcliffe <tomsci@me.com>
2022-06-02 09:56:18 +0100

    Add st7789 new init sequence

--- scripts/firstTime.sh
+++ scripts/firstTime.sh
@@ -5,7 +5,13 @@ pushd esp-iot-solution
 git reset --hard
 git apply ../esp-iot-solution.diff
 popd
+
 pushd micropython
 git reset --hard
 git apply ../micropython.diff
+popd
+
+pushd st7789
+git reset --hard
+git apply ../st7789.diff
 popd
