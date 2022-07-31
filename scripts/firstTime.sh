cd91acd14c193bdd22ebcf7b8d7f60c2b20f0ce4
Mat Booth <mat.booth@gmail.com>
2022-07-31 17:01:04 +0100

    Adds polygon drawing methods to the micropython framebuffer
    Backporting an enhancement from upstream micropython, see:
    https://github.com/micropython/micropython/issues/8987

--- scripts/firstTime.sh
+++ scripts/firstTime.sh
@@ -8,4 +8,7 @@ popd
 pushd micropython
 git reset --hard
 git apply ../micropython.diff
+
+# Backport framebuf enhancement from upstream micropython, see: https://github.com/micropython/micropython/issues/8987
+git apply ../micropython-framebuf-enhancements.patch
 popd
