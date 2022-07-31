b2c0d465d2fceb08229517c9546121b4337812ef
Mat Booth <mat.booth@gmail.com>
2022-07-31 13:19:56 +0100

    Allow successful building of dynamic natmods
    Backporting a fix from upstream micropython, see:
    https://github.com/micropython/micropython/issues/8436

--- scripts/firstTime.sh
+++ scripts/firstTime.sh
@@ -8,4 +8,7 @@ popd
 pushd micropython
 git reset --hard
 git apply ../micropython.diff
+
+# Backport fix from upstream micropython, see: https://github.com/micropython/micropython/issues/8436
+git apply ../micropython-dynamic-natmod-build-fix.patch
 popd
